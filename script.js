e69fb093fa43f815eb31d52d9173cb60f10f46da
JonaWebOfficial <jona79d3@gmail.com>
2026-08-14 21:53:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const sortSelect = document.getElementById("sortSelect");
+const container = document.querySelector(".container");
 
+sortSelect.addEventListener("change", function () {
+    const cards = Array.from(
+        document.querySelectorAll(".money-card")
+    );
+
+    cards.sort((a, b) => {
+        const difficultyA = Number(a.dataset.difficulty);
+        const difficultyB = Number(b.dataset.difficulty);
+
+        if (this.value === "low") {
+            return difficultyA - difficultyB;
+        } else {
+            return difficultyB - difficultyA;
+        }
+    });
+
+    cards.forEach(card => {
+        container.appendChild(card);
+    });
+});
